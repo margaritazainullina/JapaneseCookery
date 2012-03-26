957f83f82e15caaec1d2c75be3e0eb350d956165
Radik Zainullin <radik.zainullin@gmail.com>
2012-03-26 16:03:32 +0600

    в Recipe есть поле html - дальнейшая работа1

--- src/java/action/ImageShow.java
+++ src/java/action/ImageShow.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.apache.log4j.Logger;
 import org.apache.struts2.interceptor.ServletRequestAware;
 
-public class ShowImage extends ActionSupport implements ServletRequestAware{
+public class ImageShow extends ActionSupport implements ServletRequestAware{
     private InputStream imageStream;
     private HttpServletRequest request;
 
@@ -24,7 +24,7 @@ public class ShowImage extends ActionSupport implements ServletRequestAware{
     public String execute() throws Exception {
 //        Logger log = Logger.getLogger("common");
 //        log.info(request.getParameter("src"));
-        imageStream = ShowImage.class.getResourceAsStream("/misc/" + request.getParameter("src"));
+        imageStream = ImageShow.class.getResourceAsStream("/misc/" + request.getParameter("src"));
         return SUCCESS;
     }
 
