318f8d3e62a690a95c37787663f77ad5dee51df2
Radik Zainullin <radik.zainullin@gmail.com>
2012-05-13 22:55:10 +0600

    показ рецептов-xml с фото

--- web/js/recipies/showRecipies.js
+++ web/js/recipies/showRecipies.js
@@ -1,7 +1,7 @@
 define(["dojo/domReady!"],
     function() {
         return {
-            bar : function(domDoc, node) {
+            bar : function(domDoc, node, imageStr) {
                 require(["dojo/_base/array", "dojo/dom-construct"], 
                     function(array, domConstruct){
                         var table = domConstruct.create("table", null ,node);
@@ -11,27 +11,23 @@ define(["dojo/domReady!"],
                         domConstruct.create("td", {                            
                             innerHTML: domDoc.getElementsByTagName("info")[0].firstChild.data
                         }, domConstruct.create("tr", null, table));
-                        domConstruct.create("td", {                            
-                            innerHTML: "Ингредиенты"
+                        domConstruct.create("td", { innerHTML: "Ингредиенты"
                         }, domConstruct.create("tr", null, table));
                         array.forEach(domDoc.getElementsByTagName("ingredient"), function(entry, i){
-                            domConstruct.create("td", {                            
-                                innerHTML: entry.firstChild.data
+                            domConstruct.create("td", { innerHTML: entry.firstChild.data
                             }, domConstruct.create("tr", null, table));
                         });
-                        domConstruct.create("td", {                            
-                            innerHTML: "Готовка"
-                        }, domConstruct.create("tr", null, table));
+                        domConstruct.create("td", { innerHTML: "Готовка" }, 
+                            domConstruct.create("tr", null, table));
                         array.forEach(domDoc.getElementsByTagName("step"), function(entry, i){
-                            domConstruct.create("td", {                            
-                                innerHTML: entry.firstChild.data
+                            domConstruct.create("td", { innerHTML: entry.firstChild.data
                             }, domConstruct.create("tr", null, table));
                         });
-                        domConstruct.create("td", {                            
-                            innerHTML: "Фото"
-                        }, domConstruct.create("tr", null, table));
+                        var tdPhoto = domConstruct.create("td", null, domConstruct.create("tr", null, table));
+                        domConstruct.create("img", {  
+                            src: "data:image/jpg/png/gif;base64," + imageStr, width: "300px"
+                        }, tdPhoto);
                     });  
             }
         }
-    }
-    )
+    })
