c89d6da775658ab96fcd2e063eb76ebbe262f2ca
Radik Zainullin <radik.zainullin@gmail.com>
2012-05-15 21:24:30 +0600

    показ рецептов-xml с фото на my_recipies

---
+++ miscellaneous/db.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE `category` (
+	`id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+	`info` VARCHAR(255) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+INSERT INTO `category`(`id`, `info`) VALUES(1, 'супы'),(2, 'лапша'),(3, 'суши'),(4, 'десерты'),(5, 'другое');
